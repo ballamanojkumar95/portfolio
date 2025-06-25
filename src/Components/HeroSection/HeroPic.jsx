91bbdae497fdd5c807f330eaa9255393fa294755
ballamanojkumar95 <manojkrballa@gmail.com>
2025-06-25 09:37:48 +0530

    hero image added

--- src/Components/HeroSection/HeroPic.jsx
+++ src/Components/HeroSection/HeroPic.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { fadeIn } from '../../framer-motion/Variant';
 import { motion } from 'framer-motion';
 import { BsHexagon } from "react-icons/bs";
+import hero from '../../../public/images/hexbio.png'
 
 
 
@@ -10,7 +11,7 @@ const HeroPic = () => {
   return (
     <motion.div 
     variants={fadeIn('left',0.2)} initial="hidden" viewport={{once:false,amount:0}} whileInView="show" className='flex items-center justify-center h-full '>
-       <img src="../../public/images/hexbio.png" alt="" className='max-h-[400px] w-auto' />
+       <img src={hero} alt="" className='max-h-[400px] w-auto' />
        {/* <img src="../../public/images/circle.png"  className='h-[500px] w-[500px] absolute animate-[spin_20s_ease-in-out_infinite] z-[-1]' alt="" /> */}
        <BsHexagon  className='h-[400px] w-[400px] absolute animate-[spin_20s_ease-in-out_infinite] z-[-1]' color='rgb(0 101 164 / 46%)' />
     </motion.div>
